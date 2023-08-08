3792719c4e5f966b331df1c928090cfcf1e23753
akirsch1 <andre.kirsch@fh-bielefeld.de>
2023-08-08 15:04:47 +0200

    Update JUnit metric related stuff

--- hello-world/src/test/java/MainTest.java
+++ hello-world/src/test/java/MainTest.java
@@ -1,11 +1,21 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class MainTest {
 
     @Test
     public void getMessageCorrect() {
         assertEquals("Hello World!", Main.getMessage());
+    }
+
+    @Test
+    public void testAlwaysTrue() {
+        assertTrue(true);
+    }
+    
+    @Test
+    public void testThrowException() {
+        int x = 5 / 0;
     }
 }
