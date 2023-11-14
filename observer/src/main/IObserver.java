8d9dedf86d39f999f0ae077deca262304269f548
akirsch1 <andre.kirsch@fh-bielefeld.de>
2023-11-14 18:42:54 +0100

    Add visitor and observer tasks

---
+++ observer/src/main/IObserver.java
@@ -0,0 +1,8 @@
+public interface IObserver {
+    /**
+     * Das Verhalten wenn ein WarenTyp sich beim zu beobachten Objekt geändert hat.
+     *
+     * @param typ der typ der Ware welcher sich verändert hat
+     */
+    void update(WarenTyp typ);
+}
