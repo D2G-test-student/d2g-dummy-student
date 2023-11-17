ecb6f63e8941218b99f8bcec6b34046a795c6c78
akirsch1 <andre.kirsch@fh-bielefeld.de>
2023-11-17 13:34:25 +0100

    Removed tasks for testing

--- hello-world/src/main/java/Main.java
+++ hello-world/src/main/java/Main.java
@@ -1,9 +1,20 @@
+/**
+ * Main class.
+ */
 public class Main {
 
+    /**
+     * Returns the string "Hello World!".
+     *
+     * @return message string
+     */
     public static String getMessage() {
         return null;
     }
 
+    /**
+     * Main method of the program.
+     */
     public static void main(String[] args) {
         System.out.println(getMessage());
     }
